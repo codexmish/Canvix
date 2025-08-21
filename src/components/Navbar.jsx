b58fe23014b5fee23d8ab6c681cba5bf9fd6bb7a
codexmish <majharulislam.shanto.70@gmail.com>
2025-08-21 15:33:41 +0600

    Navbar Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,22 +20,22 @@ const Navbar = () => {
     ]
   return (
     <>
-    <nav id='Navbar'>
+    <nav id='Navbar' className='pt-[22px]'>
         <div className="container">
-            <div className="main-row">
+            <div className="main-row flex items-center justify-between">
                 <div className="logo">
                     <img src={logo} alt="logo" />
                 </div>
-                <div className="items">
+                <div className="items flex gap-[35px]">
                     {
                         navItems.map((item , i)=>(
 
-                            <Link key={i} className='text-xl font-normal text-primary font-DM' to={'/'}>{item.navContent}</Link>
-                            
+                            <Link key={i} className='text-xl hover:text-brand font-normal text-primary font-DM' to={item.navLinks}>{item.navContent}</Link>
+
                         ))
                     }
                 </div>
-                <button>Get in touch</button>
+                <button className='w-[140px] h-[47px] bg-primary rounded-[23px] text-lg font-DM font-normal'>Get in touch</button>
             </div>
         </div>
     </nav>
