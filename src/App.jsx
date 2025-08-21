301fbcfb913026e331b0f3282ea295a7daa611ae
codexmish <majharulislam.shanto.70@gmail.com>
2025-08-21 14:33:51 +0600

    Ready to go

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import './App.css'
+import { createBrowserRouter, createRoutesFromElements } from 'react-router'
 
 const App = () => {
+
+  const myRoute = createBrowserRouter(createRoutesFromElements(
+
+    
+  ))
   return (
     <>
     
