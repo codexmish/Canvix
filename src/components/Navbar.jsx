8da9df693d3d1e186e236a93fa7a2c72309b01b8
codexmish <majharulislam.shanto.70@gmail.com>
2025-08-22 19:12:53 +0600

    DropDown Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../assets/Images/logo.png'
 import { Link } from 'react-router'
+import { IoMdArrowDropup } from "react-icons/io";
 
 const Navbar = () => {
 
@@ -15,7 +16,21 @@ const Navbar = () => {
         },
         {
             navContent: 'Contact Us',
-            navLinks: '/'
+            navLinks: '/',
+            dropDown:[
+                {
+                    dropContent: 'Location',
+                    dropLink: '/'
+                },
+                {
+                    dropContent: 'Phone',
+                    dropLink: '/'
+                },
+                {
+                    dropContent: 'Social',
+                    dropLink: '/'
+                }
+            ]
         },
     ]
   return (
@@ -28,11 +43,36 @@ const Navbar = () => {
                 </div>
                 <div className="items flex gap-[35px]">
                     {
-                        navItems.map((item , i)=>(
+                        navItems.map((item , i)=>{
+                            return(
+                                <ul>
+                                    <li className='relative group'>
 
-                            <Link key={i} className='text-xl hover:text-brand font-normal text-primary font-DM' to={item.navLinks}>{item.navContent}</Link>
+                                    <Link key={i} className='text-xl hover:text-brand font-normal text-primary font-DM' to={item.navLinks}>{item.navContent}</Link>
+                                    
 
-                        ))
+                                    {
+                                        item.dropDown&&
+                                        <div className='w-[300px] p-3 rounded-[5px] bg-white absolute top-8 invisible opacity-0 group-hover:visible group-hover:opacity-[100%]' >
+                                            <IoMdArrowDropup className='text-2xl text-white absolute top-[-15px] left-5'/>
+                                            <ul>
+                                                {
+                                                    item.dropDown.map((item , i)=>(
+
+                                                        <li><Link className='text-lg inline-block mb-5 font-DM' key={i} to={item.dropLink}>{item.dropContent}</Link></li>
+                                                    ))
+                                                }
+                                            </ul>
+                                        </div>
+                                        
+                                    }
+                                    </li>
+                                </ul>
+                            )
+                        }
+
+
+                        )
                     }
                 </div>
                 <button className='w-[140px] h-[47px] bg-primary hover:bg-brand rounded-[23px] text-lg font-DM font-normal'>Get in touch</button>
